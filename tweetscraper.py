08ef3d3c794c08ce742b105affcf8ed4f2f74441
orzymandias <ganwancheng@gmail.com>
2020-05-30 14:54:09 +0800

    anger dataset

--- tweetscraper.py
+++ tweetscraper.py
@@ -2,7 +2,8 @@ import GetOldTweets3 as got
 import numpy as np
 
 text_query = 'angry'
-count = 1000
+count = 3000
+file_name = 'angry3'
 tweetCriteria = got.manager.TweetCriteria().setQuerySearch(text_query)\
                                            .setMaxTweets(count)\
 
@@ -12,7 +13,7 @@ tweets = got.manager.TweetManager.getTweets(tweetCriteria)
 text_tweets = np.asarray([[tweet.text]for tweet in tweets])
 label = np.asarray([["anger"] for i in range(0, count)])
 text_tweets = np.append(text_tweets, label, axis=1)
-np.savetxt("angry.csv", text_tweets, delimiter=",", fmt='%s')
+np.savetxt(f"{file_name}.csv", text_tweets, delimiter=",", fmt='%s')
 
 
 
