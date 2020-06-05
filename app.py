8c53a4c14efd6ef3ddc178d4a0fa40a719a64dc2
orzymandias <ganwancheng@gmail.com>
2020-06-05 16:09:20 +0800

    clear cache

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ def index():
         data["certainty"] = str(uncertainty)
         data["prediction"] = str(prediction[0])
         data["success"] = True
-    
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
